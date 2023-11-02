18e0bb45a8951b8c8d90e456ffc7818ac90c0d2f
SitiRirinSutarsih <sitiririnsutarsih@gmail.com>
2023-11-02 19:55:24 +0700

    From Asih

--- Siti Ririn Sutarsih/pertemuan1/DataBase/drawSQL-mysql-export-2023-11-02.sql
+++ Siti Ririn Sutarsih/pertemuan1/DataBase/drawSQL-mysql-export-2023-11-02.sql
@@ -1,8 +1,6 @@
 CREATE TABLE `Categories`(
     `id_kategori` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `anak_anak` VARCHAR(255) NOT NULL,
-    `remaja` VARCHAR(255) NOT NULL,
-    `dewasa` VARCHAR(255) NOT NULL
+    `nama_kategori` VARCHAR(255) NOT NULL
 );
 CREATE TABLE `Product`(
     `Kode_Barang` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
