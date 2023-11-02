9e4b232e42b8d14c10af0674391cde60bc1c71d7
RiyanAstriWijaya <thekingofkelassore@gmail.com>
2023-11-02 18:42:10 +0700

    From Riyan

---
+++ Riyan/pertemuan1/Config/Connection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace pertemuan1.Config
+{
+    class Connection
+    {
+        private static Connection instance;
+        private static MySqlConnection connection;
+        private readonly string url = "datasource=localhost;username=root;password=;database=store;Convert Zero Datetime=True";
+
+        private Connection()
+        {
+            try
+            {
+                connection = new MySqlConnection(this.url);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+
+        public MySqlDataReader Query(string sql)
+        {
+            try
+            {
+                MySqlDataReader response;
+                connection.Close();
+                connection.Open();
+                MySqlCommand query = new MySqlCommand(sql, getConnection());
+                query.ExecuteNonQuery();
+                response = query.ExecuteReader();
+                return response;
+            }
+            catch (Exception err){
+                MessageBox.Show(err.Message);
+            }
+            return null;
+        }
+
+        public static Connection getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Connection();
+            }
+            return instance;
+
+        }
+        
+    }
+}
