f0ddf144c42bf6040b2b1ff18e0841e19caca3ad
SellyMellyanaDewi <146691326+SellyMellyanaDewi@users.noreply.github.com>
2023-11-04 13:49:34 +0700

    Delete Selly Mellyana Dewi directory

--- Selly Mellyana Dewi/pertemuan1/Database/drawSQL-mysql-export-2023-11-01.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `kategori_produk`(
-    `id_kategori` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `jenis_kategori` VARCHAR(255) NOT NULL
-);
-CREATE TABLE `produk`(
-    `kode_barang` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nama_barang` VARCHAR(255) NOT NULL,
-    `stok` INT NOT NULL
-);
